17bcdf4093132e27b293b11d4d8f4600da058e3f
Jazz <jazzygermosen@gmail.com>
2025-10-02 19:44:07 -0400

    This is my first part of my solution to the highscore exercise

--- src/main/java/com/pluralsight/HighScore.java
+++ src/main/java/com/pluralsight/HighScore.java
@@ -13,14 +13,29 @@ public class HighScore {
         // print the winner
         // this is the first question
         System.out.print("What was the score of the game?: ");
-        String score = askQuestion("what is the score of the game?: ");
+        String match = askQuestion("what is the score of the game?: ");
         // lets create a method for this
         // create a print statement for the score
         // System.out.println("Winner: " + score);
         // this is where we would split the answer given and then have the score printed as Winner:
         // i think i need another method but im gonna figure out how to do that later
         // gonna create an array named scoreSpl
-        String[] winner= score.split(":|");
+        String[] teamScores = match.split("\\|");
+        String[] teams = teamScores[0].split(":");
+        String[] scores = teamScores[1].split(":");
+        int scoreA = Integer.parseInt(scores[0]);
+        int scoreB = Integer.parseInt(scores[1]);
+
+        if (scoreA > scoreB){
+            System.out.println("Winner: " + teams[0]);
+        }
+        else if (scoreB > scoreA){
+            System.out.println("Winner: " + teams[1]);
+        }
+        else {
+            System.out.println("It's a tie! ");
+        }
+
         /*switch (winner.length){
             case 1:
                 System.out.println("winner" + winner[0]);
@@ -36,21 +51,12 @@ public class HighScore {
         // i have to change ^ this to an if statement to compare the two numbers to determin the winner
         // if position 2 is > position 3 then print winner position 0
         // i think that i need to call on
-        if (winner[2] > winner[3]){
-            System.out.println("Winner: " + winner[0]);
-        }
-        else{
-            System.out.println("Winner: " + winner[1]);
-        }
-
-
-
 
     }
 
     public static String askQuestion (String askQuestion ){
         System.out.println(askQuestion);
-        String score = scoreHigh.nextLine();
-        return score.trim();
+        String match = scoreHigh.nextLine();
+        return match.trim();
     }
 }
